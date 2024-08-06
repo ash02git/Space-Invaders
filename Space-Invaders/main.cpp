f88b552e85af2ace0fb8b76db0835dbd43952442
ash02git <ashwinabraham2001@gmail.com>
2024-08-06 22:13:14 +0530

    Made 3 shapes assignment

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -19,7 +19,30 @@ int main()
                 rWindow.close();
         }
 
-        rWindow.clear(sf::Color::Blue);
+        rWindow.clear(sf::Color::Black);
+
+        //Drawing a circle
+        sf::CircleShape circleObject(50);
+        circleObject.setFillColor(sf::Color::Green);
+        //circleObject.setPosition(400,300);//using pixel value
+        //circleObject.setPosition(rWindow.getSize().x, rWindow.getSize().y);//bottom right
+        circleObject.setPosition(0, 0);
+        rWindow.draw(circleObject);
+
+        sf::RectangleShape rectangleObject;
+        rectangleObject.setFillColor(sf::Color::Red);
+        rectangleObject.setSize(sf::Vector2f(50,50));
+        rectangleObject.setPosition(400, 400);
+        rWindow.draw(rectangleObject);
+
+        sf::ConvexShape triangleObject;
+        triangleObject.setFillColor(sf::Color::Blue);
+        triangleObject.setPointCount(3);
+        triangleObject.setPosition(200, 400);
+        triangleObject.setPoint(0, sf::Vector2f(100, 100));
+        triangleObject.setPoint(1, sf::Vector2f(0, 100));
+        triangleObject.setPoint(2, sf::Vector2f(100, 0));
+        rWindow.draw(triangleObject);
 
         rWindow.display();
     }
